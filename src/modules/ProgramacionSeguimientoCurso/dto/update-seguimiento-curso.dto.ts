b1140ac5c0b9c65a6761d3e063677fdef5201075
Alexiis <okalexiiis@gmail.com>
2025-08-11 16:40:10 -0600

    feat(ProgramacionSeguimientoCurso): agregar nuevos campos al DTO de actualización de seguimiento de curso
    Se han añadido los campos 'fechaRevision', 'fechaSeguimientoFinal' y 'numeroRevision' al DTO de actualización de seguimiento de curso. Estos nuevos campos permiten una mejor gestión de la información relacionada con las revisiones y seguimientos, mejorando así la funcionalidad del sistema.

--- src/modules/ProgramacionSeguimientoCurso/dto/update-seguimiento-curso.dto.ts
+++ src/modules/ProgramacionSeguimientoCurso/dto/update-seguimiento-curso.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
 import { CreateSeguimientoCursoDto } from './create-seguimiento-curso.dto';
-import { IsOptional, IsEnum } from 'class-validator';
+import { IsOptional, IsEnum, IsDate, IsNumber } from 'class-validator';
 import { EstadoSeguimiento } from '../entities/seguimiento-curso.entity';
 
 export class UpdateSeguimientoCursoDto extends PartialType(
@@ -13,4 +13,19 @@ export class UpdateSeguimientoCursoDto extends PartialType(
   @IsOptional()
   @IsEnum(EstadoSeguimiento)
   estado?: EstadoSeguimiento;
+
+  @ApiPropertyOptional({ description: 'Fecha de revisión' })
+  @IsOptional()
+  @IsDate()
+  fechaRevision?: Date;
+
+  @ApiPropertyOptional({ description: 'Fecha de seguimiento final' })
+  @IsOptional()
+  @IsDate()
+  fechaSeguimientoFinal?: Date;
+
+  @ApiPropertyOptional({ description: 'Número de revisión' })
+  @IsOptional()
+  @IsNumber()
+  numeroRevision?: number;
 }
